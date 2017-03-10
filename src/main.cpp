a473eba59b8ca412c6d7ee826558dc737959ddab
anhmt90 <anhmt90@gmail.com>
2017-03-10 18:21:04 +0100

    done!

--- src/main.cpp
+++ src/main.cpp
@@ -21,49 +21,12 @@ using namespace std;
 #include "Schema.hpp"
 #include "Transaction.hpp"
 
-void run(int i){
-	std::this_thread::sleep_for(std::chrono::seconds(0));
-	Transaction* T = new Transaction(i);
-}
+//void run(int i){
+//	std::this_thread::sleep_for(std::chrono::seconds(0));
+//	Transaction* T = new Transaction(i);
+//}
 
 int main() {
-//	warehouse = *(new Warehouse());
-//	for(auto &w : warehouse.pk_index){
-//		cout << w.second.begin << " " << w.second.end << " | " << w.second.w_id <<"\t" << w.second.w_street_1 << "\n";
-//	}
-
-//	run(3);
-
-//	for(int i=0; i<2; ++i){
-//		run(i);
-//	}
-
-//	thread T1(run,1);
-//	thread T2(run,2);
-
-//	thread T1(run,2);
-//	std::this_thread::sleep_for(std::chrono::milliseconds(2500));
-//	thread T2(run,1);
-//
-//	T1.join();
-//	T2.join();
-
-
-
-//	orderline = *(new OrderLine());
-//	district = *(new District());
-//	uint64_t c = 0;
-//	cout << "OrderLine: " << orderline.pk_index.size() << " tuples\n\n";
-//	auto start=high_resolution_clock::now();
-//
-//	for(size_t i = 0; i<1000t; i++){
-//		for(auto V : orderline.pk_index){
-//			++c;
-//		}
-//	}
-//	auto end = duration_cast<duration<double>>(high_resolution_clock::now()-start).count();
-//	cout << "Scanned " << c << " records in " << end << "s\n";
-//	cout << "Speed: " << c/(double)end << " lookups/s\n";
 	cout << endl;
 	cout << "Order: " << order.pk_index.size() << " tuples\n";
 	cout << "NewOrder: " << neworder.pk_index.size() << " tuples\n";
